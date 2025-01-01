e2983949ff1f079edad3b52a7dfe8bdc53801ff3
Beorlor <130129090+Beorlor@users.noreply.github.com>
2025-01-02 00:44:17 +0100

    fix ex00

--- Day_01/ex00/Hello.py
+++ Day_01/ex00/Hello.py
@@ -1,7 +1,17 @@
-ft_list = ["Hello", "World!"]
-ft_tuple = ("Hello", "France!")
-ft_set = {"Hello", "Paris!"}
-ft_dict = {"Hello": "42Paris!"}
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+         
+ft_list[1] = "World!"
+
+ft_tuple = (ft_tuple[0], "France!")
+
+ft_set.remove("tutu!")   # remove the old string
+ft_set.add("Paris!")     # add the new string
+
+# For dict, change the value associated with the key "Hello"
+ft_dict["Hello"] = "42Paris!"
 
 print(ft_list)
 print(ft_tuple)
--- Day_01/ex01/format_ft_time.py
+++ Day_01/ex01/format_ft_time.py
@@ -1,7 +1,7 @@
-from datetime import datetime
+    from datetime import datetime
 
-# Temps écoulé depuis le 1er janvier 1970
-timestamp = datetime.now().timestamp()
+    # Temps écoulé depuis le 1er janvier 1970
+    timestamp = datetime.now().timestamp()
 
-print(f"Seconds since January 1, 1970: {timestamp:.4f} or {timestamp:.2e} in scientific notation")
-print(datetime.now().strftime("%b %d %Y"))
+    print(f"Seconds since January 1, 1970: {timestamp:.4f} or {timestamp:.2e} in scientific notation")
+    print(datetime.now().strftime("%b %d %Y"))
