74c9fcf2af9de964a0535bd150b3cb35bc4abeaa
Beorlor <jbdurand2000@gmail.com>
2025-01-05 06:32:48 +0100

    fix ex04 day01

--- Day_01/ex04/whatis.py
+++ Day_01/ex04/whatis.py
@@ -1,22 +1,25 @@
 import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]  # everything after the script name
-
-    if len(args) == 0:
-        sys.exit(0)
-
-    if len(args) > 1:
-        raise AssertionError("more than one argument is provided")
-
-    single_arg = args[0]
-
     try:
-        number = int(single_arg)
-    except ValueError:
-        raise AssertionError("argument is not an integer")
+        args = sys.argv[1:]  # everything after the script name
 
-    if number % 2 == 0:
-        print("I'm Even.")
-    else:
-        print("I'm Odd.")
+        if len(args) == 0:
+            sys.exit(0)
+
+        if len(args) > 1:
+            raise AssertionError("more than one argument is provided")
+
+        single_arg = args[0]
+
+        try:
+            number = int(single_arg)
+        except ValueError:
+            raise AssertionError("argument is not an integer")
+
+        if number % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
